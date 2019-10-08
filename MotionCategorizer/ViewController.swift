595739d2883268416a7d86c40e6277bbe9dd7016
Brad Howes <bradhowes@mac.com>
2019-10-08 22:12:23 +0200

    Initial Commit

---
+++ MotionCategorizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionCategorizer
+//
+//  Created by Brad Howes on 10/8/19.
+//  Copyright © 2019 Brad Howes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
