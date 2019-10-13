35931c767b10fb6c445b07ca04b73098a09e572a
Brad Howes <bradhowes@mac.com>
2019-10-13 02:13:21 +0200

    Fixed non-existent tests.

--- MotionCollectorTests/MotionCollectorTests.swift
+++ MotionCollectorTests/MotionCollectorTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import MotionCategorizer
+@testable import MotionCollector
 
-class MotionCategorizerTests: XCTestCase {
+class MotionCollectorTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
